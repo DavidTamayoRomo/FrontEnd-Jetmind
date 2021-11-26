910bb2d2816d35d65bcc3fe098d4394b3ad76be5
BrayanValdesG <balexis41@gmail.com>
2021-11-26 08:29:58 -0500

    obtener información de step representante

--- src/app/pages/contrato/contrato.component.ts
+++ src/app/pages/contrato/contrato.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { of } from 'rxjs';
-import { NgWizardConfig, NgWizardService, StepChangedArgs, StepValidationArgs, STEP_STATE, THEME } from 'ng-wizard';
-import { FormBuilder, Validators } from '@angular/forms';
+import {
+  NgWizardConfig,
+  NgWizardService,
+  StepChangedArgs,
+  StepValidationArgs,
+  STEP_STATE,
+  THEME,
+} from 'ng-wizard';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-contrato',
   templateUrl: './contrato.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ContratoComponent implements OnInit {
-
   stepStates = {
     normal: STEP_STATE.normal,
     disabled: STEP_STATE.disabled,
     error: STEP_STATE.error,
-    hidden: STEP_STATE.hidden
+    hidden: STEP_STATE.hidden,
   };
 
   config: NgWizardConfig = {
@@ -23,18 +28,28 @@ export class ContratoComponent implements OnInit {
     theme: THEME.dots,
     toolbarSettings: {
       toolbarExtraButtons: [
-        { text: 'Terminar', class: 'btn btn-info', event: () => { alert('OK'); } }
+        {
+          text: 'Terminar',
+          class: 'btn btn-info',
+          event: () => {
+            alert('OK');
+          },
+        },
       ],
-    }
+    },
   };
+
+  public executeNextRepresentante: boolean | any;
+
+  step: any;
+  dataRepresentante: any;
 
   constructor(
     private ngWizardService: NgWizardService,
-    private fb: FormBuilder,
-    ) { }
+    private fb: FormBuilder
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   public registerForm = this.fb.group({
     fecha: [null],
@@ -53,44 +68,55 @@ export class ContratoComponent implements OnInit {
     fechaAprobacion: [null],
   });
 
-  crearContrato(){
-
-  }
+  crearContrato() {}
 
   showPreviousStep(event?: Event) {
-    console.log(event);
+    // console.log(event);
     this.ngWizardService.previous();
   }
- 
+
   showNextStep(event?: Event) {
-    console.log(event);
+    // console.log(event);
     this.ngWizardService.next();
   }
- 
+
   resetWizard(event?: Event) {
-    console.log(event);
+    // console.log(event);
     this.ngWizardService.reset();
   }
- 
+
   setTheme(theme: THEME) {
     this.ngWizardService.theme(theme);
   }
- 
+
   stepChanged(args: StepChangedArgs) {
-    console.log(args.step);
-    //console.log(args);
+    // console.log(args.step);
+    this.step = args.step;
   }
- 
+
   isValidTypeBoolean: boolean = true;
- 
+
   isValidFunctionReturnsBoolean(args: StepValidationArgs) {
-    console.log(args);
+    // console.log('QE', args);
+    this.executeNextRepresentante = true;
+    setTimeout(() => {
+      this.executeNextRepresentante = false;
+    }, 1000);
     return true;
   }
- 
+
   isValidFunctionReturnsObservable(args: StepValidationArgs) {
-    console.log(args);
+    // console.log(args);
     return of(true);
   }
 
+  setDataFormRepresentante(event: any) {
+    this.dataRepresentante = event;
+    console.log(this.dataRepresentante);
+  }
+
+  sendFormValidRepresentante(event: any) {
+    // console.log('Hola', event);
+    this.config.keyNavigation = event;
+  }
 }
--- src/app/pages/representante/representante.component.ts
+++ src/app/pages/representante/representante.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { Representante } from './representante.model';
 import { FormBuilder, Validators } from '@angular/forms';
 import { FileUploadService } from '../../services/file-upload.service';
@@ -10,16 +17,18 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-representante',
   templateUrl: './representante.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class RepresentanteComponent implements OnInit {
-
+export class RepresentanteComponent implements OnInit, OnChanges {
   public representanteSeleccionada: any;
 
-  public mostrarBoton:boolean = false;
+  public mostrarBoton: boolean = false;
 
   RepresenteModel = new Representante();
+
+  @Input() executeNext: any;
+  @Output() sendFormData: EventEmitter<any> = new EventEmitter();
+  @Output() sendFormValid: EventEmitter<any> = new EventEmitter();
 
   constructor(
     private fb: FormBuilder,
@@ -27,20 +36,25 @@ export class RepresentanteComponent implements OnInit {
     private fileUploadService: FileUploadService,
     private activatedRoute: ActivatedRoute,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(({ id }) => {
       this.cargarRepresentantebyId(id);
     });
-    if(this.router.url == '/representante/nuevo'){
+    if (this.router.url == '/representante/nuevo') {
       this.mostrarBoton = true;
     }
+    this.registerForm.valueChanges.subscribe((res) => {
+      this.sendFormValid.emit(this.registerForm.valid);
+    });
   }
 
-
-
-  
+  ngOnChanges() {
+    if (this.executeNext) {
+      this.sendFormData.emit(this.registerForm.value);
+    }
+  }
 
   LlenarForm(resp: any) {
     this.RepresenteModel = resp.data;
@@ -56,7 +70,7 @@ export class RepresentanteComponent implements OnInit {
       lugarTrabajo,
       numeroEmergencia,
       telefonoOficina,
-      telefonoDomicilio
+      telefonoDomicilio,
     } = resp.data;
 
     this.registerForm.setValue({
@@ -71,25 +85,23 @@ export class RepresentanteComponent implements OnInit {
       lugarTrabajo,
       numeroEmergencia,
       telefonoOficina,
-      telefonoDomicilio
-    })
-
+      telefonoDomicilio,
+    });
   }
-
 
   public registerForm = this.fb.group({
     nombresApellidos: [null, Validators.required],
     email: [null, Validators.required],
     cedula: [null, Validators.required],
-    telefono:[null, Validators.required],
+    telefono: [null, Validators.required],
     fechaNacimiento: [null, Validators.required],
-    direccion:[null],
-    genero:[null],
-    estado:[null],
-    lugarTrabajo:[null],
-    numeroEmergencia:[null],
-    telefonoOficina:[null],
-    telefonoDomicilio:[null]
+    direccion: [null],
+    genero: [null],
+    estado: [null],
+    lugarTrabajo: [null],
+    numeroEmergencia: [null],
+    telefonoOficina: [null],
+    telefonoDomicilio: [null],
   });
 
   async cargarRepresentantebyId(id: string) {
@@ -97,30 +109,31 @@ export class RepresentanteComponent implements OnInit {
       return;
     }
 
-    this.representanteService.obtenerRepresentanteById(id)
+    this.representanteService
+      .obtenerRepresentanteById(id)
       .subscribe((resp: any) => {
         this.representanteSeleccionada = resp.data;
         this.LlenarForm(resp);
       });
-
   }
 
   cancelarGuardado() {
-    this.router.navigateByUrl('/listapersonas')
+    this.router.navigateByUrl('/listapersonas');
   }
 
-
   campoNoValido(campo: any): boolean {
-    if (this.registerForm.get(campo)?.invalid  && (this.registerForm.get(campo)?.dirty || this.registerForm.get(campo)?.touched)) {
+    if (
+      this.registerForm.get(campo)?.invalid &&
+      (this.registerForm.get(campo)?.dirty ||
+        this.registerForm.get(campo)?.touched)
+    ) {
       return true;
-    }
-    else {
+    } else {
       return false;
     }
   }
 
-  crearRepresentante(){
-
+  crearRepresentante() {
     if (this.representanteSeleccionada) {
       //actualizar
       this.RepresenteModel = this.registerForm.value;
@@ -133,58 +146,67 @@ export class RepresentanteComponent implements OnInit {
           timer: 3000,
           timerProgressBar: true,
           didOpen: (toast) => {
-            toast.addEventListener('mouseenter', Swal.stopTimer)
-            toast.addEventListener('mouseleave', Swal.resumeTimer)
-          }
-        })
+            toast.addEventListener('mouseenter', Swal.stopTimer);
+            toast.addEventListener('mouseleave', Swal.resumeTimer);
+          },
+        });
         Toast.fire({
           icon: 'error',
-          title: 'Verificar campos invalidos \n Indicados con el color rojo'
-        })
+          title: 'Verificar campos invalidos \n Indicados con el color rojo',
+        });
         return;
       } else {
-        
-        this.representanteService.updateRepresentante(this.representanteSeleccionada._id, this.RepresenteModel).subscribe((resp: any) => {
-          const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.addEventListener('mouseenter', Swal.stopTimer)
-              toast.addEventListener('mouseleave', Swal.resumeTimer)
+        this.representanteService
+          .updateRepresentante(
+            this.representanteSeleccionada._id,
+            this.RepresenteModel
+          )
+          .subscribe(
+            (resp: any) => {
+              const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                  toast.addEventListener('mouseenter', Swal.stopTimer);
+                  toast.addEventListener('mouseleave', Swal.resumeTimer);
+                },
+              });
+              Toast.fire({
+                icon: 'success',
+                title: 'Se actualizo correctamente',
+              });
+              this.router.navigateByUrl('/listarepresentantes');
+            },
+            (err: any) => {
+              console.warn(err.error.message);
+
+              const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                  toast.addEventListener('mouseenter', Swal.stopTimer);
+                  toast.addEventListener('mouseleave', Swal.resumeTimer);
+                },
+              });
+
+              //TODO: Mostrar error cuando es administrador. Dato que muestra el error completo=  err.error.message
+              Toast.fire({
+                icon: 'error',
+                title:
+                  'ERROR: ' +
+                  err.error.statusCode +
+                  '\nContactese con su proveedor de software ',
+              });
             }
-          })
-          Toast.fire({
-            icon: 'success',
-            title: 'Se actualizo correctamente'
-          })
-          this.router.navigateByUrl('/listarepresentantes');
-        }, (err: any) => {
-
-          console.warn(err.error.message);
-
-          const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.addEventListener('mouseenter', Swal.stopTimer)
-              toast.addEventListener('mouseleave', Swal.resumeTimer)
-            }
-          })
-
-          //TODO: Mostrar error cuando es administrador. Dato que muestra el error completo=  err.error.message
-          Toast.fire({
-            icon: 'error',
-            title: 'ERROR: ' + err.error.statusCode + '\nContactese con su proveedor de software '
-          })
-        });
+          );
       }
-    }else{
+    } else {
       //crear
       if (this.registerForm.invalid) {
         console.log('Formulario invalido');
@@ -195,66 +217,65 @@ export class RepresentanteComponent implements OnInit {
           timer: 6000,
           timerProgressBar: true,
           didOpen: (toast) => {
-            toast.addEventListener('mouseenter', Swal.stopTimer)
-            toast.addEventListener('mouseleave', Swal.resumeTimer)
-          }
-        })
+            toast.addEventListener('mouseenter', Swal.stopTimer);
+            toast.addEventListener('mouseleave', Swal.resumeTimer);
+          },
+        });
         Toast.fire({
           icon: 'error',
-          title: '- Campos con asterisco son obligatorios\n - Verificar campos invalidos, \n indicados con el color rojo  '
-        })
-        return;
-      }else{
-        this.representanteService.crearRepresentante(this.registerForm.value).subscribe((resp) => {
-          
-          const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.addEventListener('mouseenter', Swal.stopTimer)
-              toast.addEventListener('mouseleave', Swal.resumeTimer)
-            }
-          })
-          Toast.fire({
-            icon: 'success',
-            title: 'Guardado correctamente'
-          })
-
-          this.router.navigateByUrl('/listarepresentantes');
-        }, (err: any) => {
-
-          console.warn(err.error.message);
-
-          const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.addEventListener('mouseenter', Swal.stopTimer)
-              toast.addEventListener('mouseleave', Swal.resumeTimer)
-            }
-          })
-
-          //TODO: Mostrar error cuando es administrador. Dato que muestra el error completo=  err.error.message
-          Toast.fire({
-            icon: 'error',
-            title: 'ERROR: ' + err.error.statusCode + '\nContactese con su proveedor de software '
-          })
+          title:
+            '- Campos con asterisco son obligatorios\n - Verificar campos invalidos, \n indicados con el color rojo  ',
         });
+        return;
+      } else {
+        this.representanteService
+          .crearRepresentante(this.registerForm.value)
+          .subscribe(
+            (resp) => {
+              const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                  toast.addEventListener('mouseenter', Swal.stopTimer);
+                  toast.addEventListener('mouseleave', Swal.resumeTimer);
+                },
+              });
+              Toast.fire({
+                icon: 'success',
+                title: 'Guardado correctamente',
+              });
+
+              this.router.navigateByUrl('/listarepresentantes');
+            },
+            (err: any) => {
+              console.warn(err.error.message);
+
+              const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                  toast.addEventListener('mouseenter', Swal.stopTimer);
+                  toast.addEventListener('mouseleave', Swal.resumeTimer);
+                },
+              });
+
+              //TODO: Mostrar error cuando es administrador. Dato que muestra el error completo=  err.error.message
+              Toast.fire({
+                icon: 'error',
+                title:
+                  'ERROR: ' +
+                  err.error.statusCode +
+                  '\nContactese con su proveedor de software ',
+              });
+            }
+          );
       }
-      
     }
-
-    
   }
-
-  
-  
-
-
 }
