9b362549db1744cb27f77a84b2152fedacaf02be
David <david95tamayo@hotmail.es>
2022-04-12 16:40:22 -0500

    reporte estudiantes v2

--- src/app/pages/estudiante/reporte-estudiantes/reporte-estudiantes.component.ts
+++ src/app/pages/estudiante/reporte-estudiantes/reporte-estudiantes.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
+import { CiudadService } from '../../services/ciudad.service';
+import { MarcaService } from '../../services/marca.service';
 import { ProgramaService } from '../../services/programa.service';
+import { SucursalService } from '../../services/sucursal.service';
 
 @Component({
   selector: 'app-reporte-estudiantes',
@@ -9,19 +14,90 @@ import { ProgramaService } from '../../services/programa.service';
 })
 export class ReporteEstudiantesComponent implements OnInit {
 
-  public respuesta:any;
+  public respuesta: any;
+
+  public dropdownListCiudades: any = [];
+  public dropdownListSucursales: any = [];
+  public dropdownListMarcas: any = [];
+
+  public selectedItems: any = [];
+  public dropdownSettings: IDropdownSettings = {};
+  public ciudad: any = [];
+  public sucursal: any = [];
+  public marca: any = [];
 
   constructor(
     private programaService: ProgramaService,
-
+    private ciudadService: CiudadService,
+    private sucursalService: SucursalService,
+    private marcaService: MarcaService,
+    private fb: FormBuilder,
   ) { }
 
   ngOnInit(): void {
     this.obtenerDatos({
-      "idCiudad": ["613a389282cbc52ac8a87a13","618984e64fdd230e906c5ac6"],
+      "idCiudad": ["613a389282cbc52ac8a87a13", "618984e64fdd230e906c5ac6"],
       "idMarca": ["613a53447f51e7211092c8de"],
       "idSucursal": ["613a5496611f153bb0ea93f1"]
     });
+
+    /** Servicio que me devuelva las CIUDADES de la base de datos */
+    this.recuperarDatosCiudad();
+    /** Servicio que me devuelva las SUCURSALES de la base de datos */
+    this.recuperarDatosSucursales();
+    /** Servicio que me devuelva las MARCAS de la base de datos */
+    this.recuperarDatosMarcas();
+
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'nombre',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      //itemsShowLimit: 3,
+      allowSearchFilter: true
+    };
+  }
+
+
+  public registerForm = this.fb.group({
+    idMarca: [null],
+    idCiudad: [null],
+    idSucursal: [null]
+  });
+
+  recuperarDatosCiudad() {
+    this.ciudadService.getAllCiudades().subscribe((resp: any) => {
+      let nombreciudades: any = [];
+      resp.data.forEach((element: any) => {
+        nombreciudades.push({ item_id: element._id, nombre: element.nombre });
+      });
+      this.dropdownListCiudades = nombreciudades;
+
+    });
+
+  }
+  recuperarDatosSucursales() {
+    this.sucursalService.getAllSucursales().subscribe((resp: any) => {
+      let nombreSucursal: any = [];
+      resp.data.forEach((element: any) => {
+        nombreSucursal.push({ item_id: element._id, nombre: element.nombre });
+      });
+      this.dropdownListSucursales = nombreSucursal;
+
+
+    });
+
+  }
+  recuperarDatosMarcas() {
+    this.marcaService.getAllMarcas().subscribe((resp: any) => {
+      let nombremarcas: any = [];
+      resp.data.forEach((element: any) => {
+        nombremarcas.push({ item_id: element._id, nombre: element.nombre });
+      });
+      this.dropdownListMarcas = nombremarcas;
+    });
+
   }
 
 
@@ -31,6 +107,145 @@ export class ReporteEstudiantesComponent implements OnInit {
       console.log(resp);
       this.respuesta = resp.data;
     });
+  }
+
+  buscar(){
+    let datos = this.registerForm.value;
+    datos.idCiudad = this.ciudad.map((resp: any) => {
+      return resp.item_id;
+    });
+    datos.idSucursal = this.sucursal.map((resp: any) => {
+      return resp.item_id;
+    });
+    datos.idMarca = this.marca.map((resp: any) => {
+      return resp.item_id;
+    });
+    
+    this.obtenerDatos(datos);
+  }
+
+
+
+
+
+  /**
+   * ===================================================
+   *  Multi select dropDown
+   * ===================================================
+   */
+  /** CIUDAD */
+  /** Item Seleccionado */
+  onItemSelect(item: any) {
+    this.ciudad.push(item);
+    console.log(this.ciudad);
+  }
+  /** Todos los items Seleccionados */
+  onSelectAll(items: any) {
+    this.ciudad = items;
+    console.log(this.ciudad);
+  }
+  /** Deselccionar item */
+  findByItemIdIndexCiudad(id: any) {
+    return this.ciudad.findIndex((resp: any) => {
+      return resp.item_id === id;
+    })
+  }
+  onDeSelect(item: any) {
+    //verificar esto no se queda asi
+    console.log(item);
+    console.log("entre");
+    if (item.item_id) {
+      console.log("entre 1");
+      const index = this.findByItemIdIndexCiudad(item.item_id);
+      const newArray = (index > -1) ? [
+        ...this.ciudad.slice(0, index),
+        ...this.ciudad.slice(index + 1)
+      ] : this.ciudad;
+      this.ciudad = newArray;
+      console.log(this.ciudad);
+    } else {
+      console.log("entre 2");
+      const index = this.ciudad.indexOf(item);
+      const newArray = (index > -1) ? [
+        ...this.ciudad.slice(0, index),
+        ...this.ciudad.slice(index + 1)
+      ] : this.ciudad;
+      this.ciudad = newArray;
+      console.log(this.ciudad);
+    }
+  }
+
+
+  /** Deselccionar todos los items */
+  onDeSelectAll(items: any) {
+    this.ciudad = items;
+    console.log(this.ciudad);
+  }
+
+  /** SUCRUSAL */
+  /** Item Seleccionado */
+  onItemSelectsucursal(item: any) {
+    this.sucursal.push(item);
+    console.log(this.sucursal);
+  }
+  /** Todos los items Seleccionados */
+  onSelectAllsucursal(items: any) {
+    this.sucursal = items;
+    console.log(this.sucursal);
+  }
+  /** Deselccionar item */
+  findByItemIdIndexSucursal(id: any) {
+    return this.sucursal.findIndex((resp: any) => {
+      return resp.item_id === id;
+    })
+  }
+  onDeSelectsucursal(item: any) {
+    /** Borrar elemento del array  */
+    const index = this.findByItemIdIndexSucursal(item.item_id);
+    const newArray = (index > -1) ? [
+      ...this.sucursal.slice(0, index),
+      ...this.sucursal.slice(index + 1)
+    ] : this.sucursal;
+    this.sucursal = newArray;
+    console.log(this.sucursal);
+  }
+  /** Deselccionar todos los items */
+  onDeSelectAllsucursal(items: any) {
+    this.sucursal = items;
+    console.log(this.sucursal);
+  }
+
+  /** MARCA */
+  /** Item Seleccionado */
+  onItemSelectmarca(item: any) {
+    this.marca.push(item);
+    console.log(this.marca);
+  }
+  /** Todos los items Seleccionados */
+  onSelectAllmarca(items: any) {
+    this.marca = items;
+    console.log(this.marca);
+  }
+  /** Deselccionar item */
+  findByItemIdIndexMarca(id: any) {
+    return this.marca.findIndex((resp: any) => {
+      return resp.item_id === id;
+    })
+  }
+  onDeSelectmarca(item: any) {
+    /** Borrar elemento del array  */
+    const index = this.findByItemIdIndexMarca(item.item_id);
+    const newArray = (index > -1) ? [
+      ...this.marca.slice(0, index),
+      ...this.marca.slice(index + 1)
+    ] : this.marca;
+    this.marca = newArray;
+    console.log(this.marca);
+  }
+  /** Deselccionar todos los items */
+  onDeSelectAllmarca(items: any) {
+    this.marca = items;
+    console.log(this.marca);
   }
 
 }
