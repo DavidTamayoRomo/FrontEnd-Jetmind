d4179a49775389b6213185bd64576dc6ea1ba26c
David <david95tamayo@hotmail.es>
2022-04-14 16:13:19 -0500

    pdf estudiante

--- src/app/pages/estudiante/reporte-asistencia/reporte-asistencia.component.ts
+++ src/app/pages/estudiante/reporte-asistencia/reporte-asistencia.component.ts
@@ -1,7 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AsistenciaService } from '../../services/asistencia.service';
 import { EstudianteService } from '../../services/estudiante.service';
+
+
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+import jsPDF from 'jspdf';
+import htmlToPdfmake from 'html-to-pdfmake';
 
 @Component({
   selector: 'app-reporte-asistencia',
@@ -10,6 +18,8 @@ import { EstudianteService } from '../../services/estudiante.service';
   ]
 })
 export class ReporteAsistenciaComponent implements OnInit {
+
+  @ViewChild('pdfTable') pdfTable: ElementRef;
 
   estudiante: any;
   asistencias: any;
@@ -28,6 +38,18 @@ export class ReporteAsistenciaComponent implements OnInit {
       this.datosEstudiante(id);
       this.asistenciasEstudiante(id);
     });
+  }
+
+  public downloadAsPDF() {
+
+    const doc = new jsPDF();
+    const pdfTable = this.pdfTable.nativeElement;
+    var html = htmlToPdfmake(pdfTable.innerHTML);
+    const documentDefinition = { content: html };
+    pdfMake.createPdf(documentDefinition).open(); 
+
+     
+
   }
 
   datosEstudiante(idEstudiante: any) {
