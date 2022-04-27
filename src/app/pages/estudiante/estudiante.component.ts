8e8d55bac7e7aced5e7c837758c965f2819404c8
David <david95tamayo@hotmail.es>
2022-04-27 17:42:26 -0500

    validar cedula

--- src/app/pages/estudiante/estudiante.component.ts
+++ src/app/pages/estudiante/estudiante.component.ts
@@ -37,6 +37,9 @@ export class EstudianteComponent implements OnInit, OnChanges {
 
   EstudianteModel = new Estudiante();
 
+  public validador:any;
+  public mensajeCedula:any = '';
+
   @Input() executeNext: any;
   @Input() executeEnter: any;
   @Output() sendFormData: EventEmitter<any> = new EventEmitter();
@@ -387,4 +390,100 @@ export class EstudianteComponent implements OnInit, OnChanges {
   onDeSelectAllRepresentante(items: any) {
     this.representante = items;
   }
+
+
+   //esta es la variable de validación
+  
+   validadorDeCedula() {
+    let cedula:string = this.registerForm.get('cedula').value.toString();
+
+    let cedulaCorrecta = false;
+    //Preguntamos si la cedula consta de 10 digitos
+    if (cedula.length >= 10) {
+
+      //Obtenemos el digito de la region que sonlos dos primeros digitos
+      var digito_region: any = cedula.substring(0, 2);
+
+      //Pregunto si la region existe ecuador se divide en 24 regiones
+      if (digito_region >= 1 && digito_region <= 24) {
+
+        // Extraigo el ultimo digito
+        var ultimo_digito = cedula.substring(9, 10);
+
+        //Agrupo todos los pares y los sumo
+        var pares = parseInt(cedula.substring(1, 2)) + parseInt(cedula.substring(3, 4)) + parseInt(cedula.substring(5, 6)) + parseInt(cedula.substring(7, 8));
+
+        //Agrupo los impares, los multiplico por un factor de 2, si la resultante es > que 9 le restamos el 9 a la resultante
+        var numero1: any = cedula.substring(0, 1);
+        numero1 = (numero1 * 2);
+        if (numero1 > 9) {
+          numero1 = (numero1 - 9);
+        }
+
+        var numero3: any = cedula.substring(2, 3);
+        numero3 = (numero3 * 2);
+        if (numero3 > 9) { numero3 = (numero3 - 9); }
+
+        var numero5: any = cedula.substring(4, 5);
+        numero5 = (numero5 * 2);
+        if (numero5 > 9) { numero5 = (numero5 - 9); }
+
+        var numero7: any = cedula.substring(6, 7);
+        numero7 = (numero7 * 2);
+        if (numero7 > 9) { numero7 = (numero7 - 9); }
+
+        var numero9: any = cedula.substring(8, 9);
+        numero9 = (numero9 * 2);
+        if (numero9 > 9) { numero9 = (numero9 - 9); }
+
+        var impares = numero1 + numero3 + numero5 + numero7 + numero9;
+
+        //Suma total
+        var suma_total = (pares + impares);
+
+        //extraemos el primero digito
+        var primer_digito_suma = String(suma_total).substring(0, 1);
+
+        //Obtenemos la decena inmediata
+        var decena = (parseInt(primer_digito_suma) + 1) * 10;
+
+        //Obtenemos la resta de la decena inmediata - la suma_total esto nos da el digito validador
+        var digito_validador: any = decena - suma_total;
+
+        //Si el digito validador es = a 10 toma el valor de 0
+        if (digito_validador == 10)
+          digito_validador = 0;
+
+        //Validamos que el digito validador sea igual al de la cedula
+        if (digito_validador == ultimo_digito) {
+          console.log('la cedula:' + cedula + ' es correcta');
+          cedulaCorrecta = true;
+          this.validador = true;
+          return true;
+        } else {
+          console.log('la cedula:' + cedula + ' es incorrecta');
+          cedulaCorrecta = false;
+          this.validador = false;
+          return false;
+        }
+
+      } else {
+        // imprimimos en consola si la region no pertenece
+        console.log('Esta cedula no pertenece a ninguna region');
+        this.mensajeCedula = 'Esta cedula no pertenece a ninguna region';
+        cedulaCorrecta = false;
+        this.validador = false;
+        return false;
+      }
+    } else {
+      //imprimimos en consola si la cedula tiene mas o menos de 10 digitos
+      console.log('Esta cedula tiene menos de 10 Digitos');
+      this.mensajeCedula = 'Esta cedula tiene menos de 10 Digitos';
+      cedulaCorrecta = false;
+      this.validador = false;
+      return false;
+    }
+    
+  }
+
 }
